d089235ecd2c2fc64a272b6e1d97dbaff82eacb3
maxdo1511 <80486668+maxdo1511@users.noreply.github.com>
2023-01-16 18:31:54 +0300

    Set base settings to window

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,53 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+int HEIGHT = 480;
+int WIDTH = 640;
+
+void glfwWindowSizeChangeEvent(GLFWwindow* window, int width, int height)
+{
+    HEIGHT = height;
+    WIDTH = width;
+    glViewport(0, 0, WIDTH, HEIGHT);
+    std::cout << "Window change!" << std::endl;
+}
+
+void glfwKeyPressedEvent(GLFWwindow* window, int key, int scanecode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE)
+    {
+        if (action == GLFW_PRESS)
+        {
+            glfwSetWindowShouldClose(window, GL_TRUE);
+        }
+    }
+}
+
 int main(void)
 {
-    GLFWwindow* window;
 
-    /* Initialize the library */
     if (!glfwInit())
+    {
+        std::cout << "glfw no Init!" << std::endl;
         return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Hello World", NULL, NULL);
+
     if (!window)
     {
+        std::cout << "No window!" << std::endl;
         glfwTerminate();
         return -1;
     }
+
+    glfwSetWindowSizeCallback(window, glfwWindowSizeChangeEvent);
+    glfwSetKeyCallback(window, glfwKeyPressedEvent);
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
@@ -27,7 +59,8 @@ int main(void)
         return -1;
     }
 
-    std::cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor << std::endl;
+    std::cout << "Render " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "OpenGL " << glGetString(GL_VERSION) << std::endl;
 
     glClearColor(0, 1, 0, 1);
 
